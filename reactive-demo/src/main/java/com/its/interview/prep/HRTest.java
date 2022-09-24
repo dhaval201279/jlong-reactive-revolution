e11a6cbe3ab1eb8fa912827c6cafb54683212bde
Dhaval Shah <dhaval201279@rediffmail.com>
2022-09-24 16:23:24 +0530

    Added HRTest.java

---
+++ reactive-demo/src/main/java/com/its/interview/prep/HRTest.java
@@ -0,0 +1,33 @@
+package com.its.interview.prep;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class HRTest {
+
+    public static void main(String [] args) {
+        List<Integer> integerList = new ArrayList<Integer>();
+        integerList.add(1);
+
+        integerList.add(2);
+
+        integerList.add(3);
+
+        findNumber(integerList, 4);
+
+        System.out.println(" list : " + integerList);
+
+    }
+
+    static String findNumber(List<Integer> arr, int k) {
+        if (arr.contains(k)) {
+            return "YES";
+        }
+        else {
+            return "NO";
+        }
+
+
+
+    }
+}
